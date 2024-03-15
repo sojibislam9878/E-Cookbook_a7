03aba1ff4d8a3006c770219baf4b613a658ca47f
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-03-15 21:46:19 +0600

    file created and tailwind and daidyUI added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-600'>Vite + React</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
